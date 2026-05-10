ff86d32385ac0b2f5637510bb1e826a51660ab19
raza-dev12 <alirazamemon.dev@gmail.com>
2026-05-10 22:04:46 +0500

    new_repo

--- class1.py
+++ class1.py
@@ -1,4 +1,8 @@
 students= ["raza","Ahsan","Ik","talha","Ahmed","Mohsin","Ali","faraz"]
-for n in range(10):
-    print(n,students[n])
+# for n in range(10):
+#     print(n,students[n])
+
+for i in students:
+    print(f"-hello {i}")
+
 
