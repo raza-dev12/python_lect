beefe78543b801306ebb556ae6546a2e992d575b
raza-dev12 <alirazamemon.dev@gmail.com>
2026-05-16 03:45:47 +0500

    first_repo

---
+++ function.py
@@ -0,0 +1,64 @@
+######## python function:
+# print()
+# type()
+# int()
+# float()
+# str()
+# inpt()
+# min()
+# max
+# sum 
+# length etc ----
+#-------python class function--------- 
+# dict.pop
+#str.lower()
+# tuple.count()
+# set.differerce()
+# user defined functions
+# def say_hello():
+#     print("hello")
+# say_hello()    
+
+# def add():
+#     a=10
+#     b=20
+#     print(a+b)
+# add()    
+# add()   # more time to print call it  the function
+
+# definig a  parametrized function
+# def say_hello(name):
+#     print(f"Hello {name}")
+# say_hello('ahmed')    
+
+# def fahrenheit_to_celsius(fahrenheit):
+#   return (fahrenheit - 32) * 5 / 9
+
+# print(fahrenheit_to_celsius(77))
+# print(fahrenheit_to_celsius(95))
+# print(fahrenheit_to_celsius(50))
+
+
+def my_function(name = "friend"):
+  print("Hello", name)
+
+my_function("Emil")
+my_function("Tobias")
+my_function()
+my_function("Linus")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
